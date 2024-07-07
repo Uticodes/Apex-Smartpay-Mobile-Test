b4f34575aefbfa2636d5ca0471cc341798322157
Uticodes <etim.utibe@gmail.com>
2024-07-07 19:01:47 +0100

    Implement Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apex_smartpay_mobile_test/presentation/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'data/services/dio_wrapper/dio_client_wrapper.dart';
@@ -13,39 +14,16 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            'Welcome to Smartpay App',
-          ),
-        ),
-      ),
+      home: const SplashScreen(),
     );
   }
 }
