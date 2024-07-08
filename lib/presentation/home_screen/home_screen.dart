fd98d5d903a78e20270c0edcd8ec2d2d471fe5a9
Uticodes <etim.utibe@gmail.com>
2024-07-08 17:04:38 +0100

    Implement Home screen

--- lib/presentation/home_screen/home_screen.dart
+++ lib/presentation/home_screen/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:apex_smartpay_mobile_test/app_theme/app_theme.dart';
+import 'package:apex_smartpay_mobile_test/utils/extension_functions.dart';
 import 'package:flutter/material.dart';
+
+import '../../utils/app_text.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +14,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child:
+        Padding(
+          padding: const EdgeInsets.only(left: 24.0, right: 24),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                heightMargin(32),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: AppTheme.primaryColor,
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+                    child: TextViewBold(
+                        text: "Your Secret",
+                        color: AppTheme.white,
+                        fontSize: 16
+                    ),
+                  ),
+                ),
+                heightMargin(12),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
   }
 }
